b0deec9c55035f15063077a5dac5561bdfbca42f
jennydatson <jennydatson2@gmail.com>
2023-07-17 07:30:34 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description  main - Prints out a string to stdout.
+ * Return: 0 if Success.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcomeis a piece of art,\n");
+return (0);
+}
