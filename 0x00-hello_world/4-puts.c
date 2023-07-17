a3cc163747e64855f3eb0b9c5fe0a1ba909894ab
jennydatson <jennydatson2@gmail.com>
2023-07-17 09:10:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if Success.
+ * main -A program that prints a line with puts function
+ * Return: 0 (Success)
  */
 int main(void)
-{	
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
