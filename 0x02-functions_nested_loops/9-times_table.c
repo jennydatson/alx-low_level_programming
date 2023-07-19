6985f9b4253f3fda053471c6c36ae8bc0f210384
jennydatson <jennydatson2@gmail.com>
2023-07-19 13:52:52 -0700

    10-add.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,20 +4,20 @@ void times_table(void)
 {
 	int i, j, result;
 
-		for (i = 0 ; i < 10 ; i++)
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 10 ; j++)
 		{
-			for (j = 0 ; j < 10 ; j++)
+			result = i * j;
+			if (j == 0)
+				printf("%d, ", result);
+			else
 			{
-				result = i * j;
-				if (j == 0)
-					printf("%d, ", result);
-				else
-				{
-					printf("%2d", result);
-					if (j != 9)
-						printf(", ");
-				}
+				printf("%2d", result);
+				if (j != 9)
+					printf(", ");
 			}
-			printf("\n");
 		}
+		printf("\n");
+	}
 }
