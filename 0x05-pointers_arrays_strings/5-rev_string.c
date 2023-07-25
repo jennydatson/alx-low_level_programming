15686e74230d673e26570830ea74ecd31a4709f3
jennydatson <jennydatson2@gmail.com>
2023-07-25 09:42:42 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,8 +14,8 @@ void rev_string(char *s)
 
 	for (index = len - 1; index >= len / 2; index--)
 	{
-	tmp = s[index]
-	s[index] =s[len - index - 1]
+	tmp = s[index];
+	s[index] = s[len - index - 1];
 	s[len - index - 1] = tmp;
 	}
 
