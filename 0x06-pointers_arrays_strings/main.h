3c7007a39e29af588b97b0be03a83c2966868b60
jennydatson <jennydatson2@gmail.com>
2023-07-28 13:12:56 -0700

    mmm

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <string.h>
+#include <stdio.h>
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -13,5 +14,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 void print_number(int n);
+int main(void);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
